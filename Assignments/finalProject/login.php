77417491d71eba050eb2bcad96b057443b79926c
malassila <90357470+malassila@users.noreply.github.com>
2021-12-11 14:53:57 -0500

    Add files via upload

---
+++ Assignments/finalProject/login.php
@@ -0,0 +1,48 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>    
+    <div class='p-3 mb-2 bg-dark text-white'>
+        <div class='container'>
+            <h1><b>Login</b></h1>
+        </div>
+        
+
+
+
+
+    </div>
+    <div class='container'>
+
+        <div class='p-1 mb-1'>
+            <label for="email"><h5>Email</h5></label><br>
+            <input type="email" class='p-2' name='email' id='email' style='width: 750px'><br>
+        </div>
+
+        <div class='p-1 mb-1'>
+            <label for="pass"><h5>Password</h5></label><br>
+            <input type="password" class='p-2' name='pass' id='pass' style='width: 750px'>
+        </div>
+
+        <div class='p-1 mb-1'>
+            <button type='submit' class='btn btn-dark' name='login'><h5>Login</h5></button>
+        </div>
+
+
+    </div>
+
+    
+</body>
+</html>
